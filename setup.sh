6f457355fbf9965518820fe01920f235fa50ae7e
Puffball101961 <37782411+Puffball101961@users.noreply.github.com>
2023-02-26 20:18:43 +1100

    Release v0.1 Beta
    v0.1 Beta New Features
    - Install Script
    - Fixed stuff for raspberry pi
    - Various fixes

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+echo "Automated setup tool"
+echo "Updating System"
+sudo apt-get update && sudo apt-get upgrade -y
+echo "Installing Dependencies"
+sudo apt-get install python3 python3-dev python3-pillow libopenjp2-7-dev
+
+echo "Removing Audio Module"
+echo "blacklist snd_bcm2835" | sudo tee /etc/modprobe.d/blacklist-rgb-matrix.conf
+
+sudo update-initramfs -u
+
+echo "Reserving CPU core for Displays"
+sudo sed -i -e 's/$/ isolcpus=3/' /boot/cmdline.txt
+
+echo "Configuring System Service"
+sudo cp matrixticker.service /etc/systemd/system/matrixticker.service
+sudo systemctl enable matrixticker
+
+#echo "Rebooting"
+sudo reboot
