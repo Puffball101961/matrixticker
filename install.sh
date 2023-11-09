c2029dc265838e9de46728e04b45abece248ac02
Joel Muscat <37782411+Puffball101961@users.noreply.github.com>
2023-11-10 10:19:23 +1100

    Add automatic timezone detection

--- install.sh
+++ install.sh
@@ -11,3 +11,15 @@ sudo systemctl disable matrixticker-setup
 echo "Configuring System Service"
 sudo cp matrixticker.service /etc/systemd/system/matrixticker.service
 sudo systemctl enable matrixticker
+
+echo "Automatically Setting Timezone"
+zone=$(wget -O - -q http://geoip.ubuntu.com/lookup | sed -n -e 's/.*<TimeZone>\(.*\)<\/TimeZone>.*/\1/ p')
+
+if [ "$zone" != "" ]; then
+    echo $zone | sudo tee /etc/timezone > /dev/null
+    dpkg-reconfigure -f noninteractive tzdata >/dev/null 2>&1
+    timedatectl set-timezone $zone
+    echo "[INFO] Timezone was set to $zone" >> "$logFile"   
+else
+    echo "[ERROR] Timezone is empty" >> "$logFile"
+fi
