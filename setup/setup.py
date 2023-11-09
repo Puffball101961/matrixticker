59a0fcf416fed66721cbf773344d1ebdf723e61d
Joel Muscat <37782411+Puffball101961@users.noreply.github.com>
2023-11-09 21:45:30 +1100

    Fix Stuff

--- setup/setup.py
+++ setup/setup.py
@@ -6,13 +6,12 @@
 # directory of the repository.
 
 import time
-import os
 import subprocess
 
 from rgbmatrix import RGBMatrix, RGBMatrixOptions, graphics
 import requests
 
-from PIL import Image, ImageFont, ImageDraw
+from PIL import Image, ImageDraw
 
 
 # Configuration for the matrix
@@ -25,10 +24,6 @@ options.hardware_mapping = 'adafruit-hat-pwm'  # If you have an Adafruit HAT: 'a
 options.led_rgb_sequence = "RBG" # Change to RGB if your matrix has led colors swapped
 
 matrix = RGBMatrix(options = options)
-
-# Fonts
-# titleFont = ImageFont.load("8x13.pil")
-# textFont = ImageFont.load("6x10.pil")
 
 # Splash Screen
 splash = Image.new('RGBA', (128,32))
@@ -45,6 +40,36 @@ try:
         connected = True
 except requests.ConnectionError:
     connected = False
+
+if connected:
+    matrix.Clear()
+    splash = Image.new('RGBA', (128,32))
+    draw = ImageDraw.Draw(splash)
+    icon = Image.open(f"connected.png")
+    icon.thumbnail((128,32))
+    splash.paste(icon, (0,0))
+    matrix.SetImage(splash.convert('RGB'))
+    time.sleep(4)
+    matrix.Clear()
+    splash = Image.new('RGBA', (128,32))
+    draw = ImageDraw.Draw(splash)
+    icon = Image.open(f"installing.png")
+    icon.thumbnail((128,32))
+    splash.paste(icon, (0,0))
+    matrix.SetImage(splash.convert('RGB'))
+    subprocess.call("git clone https://github.com/Puffball101961/matrixticker.git /home/pi/matrixticker", shell=True)
+    subprocess.call("sudo git checkout unstable", cwd="/home/pi/matrixticker",shell=True)
+    subprocess.call("chmod +x /home/pi/matrixticker/install.sh", shell=True)
+    subprocess.call("sudo /home/pi/matrixticker/install.sh", cwd="/home/pi/matrixticker",shell=True)
+    matrix.Clear()
+    splash = Image.new('RGBA', (128,32))
+    draw = ImageDraw.Draw(splash)
+    icon = Image.open(f"restarting.png")
+    icon.thumbnail((128,32))
+    splash.paste(icon, (0,0))
+    matrix.SetImage(splash.convert('RGB'))
+    time.sleep(4)
+    subprocess.call("sudo reboot", shell=True)
 
 if not connected:
     splash = Image.new('RGBA', (128,32))
@@ -85,8 +110,9 @@ if not connected:
         splash.paste(icon, (0,0))
         matrix.SetImage(splash.convert('RGB'))
         subprocess.call("git clone https://github.com/Puffball101961/matrixticker.git /home/pi/matrixticker", shell=True)
+        subprocess.call("sudo git checkout unstable", cwd="/home/pi/matrixticker",shell=True)
         subprocess.call("chmod +x /home/pi/matrixticker/install.sh", shell=True)
-        subprocess.call("sudo /home/pi/matrixticker/install.sh", shell=True)
+        subprocess.call("sudo /home/pi/matrixticker/install.sh", cwd="/home/pi/matrixticker",shell=True)
         matrix.Clear()
         splash = Image.new('RGBA', (128,32))
         draw = ImageDraw.Draw(splash)
